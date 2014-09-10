afd3f38942646923d70a0cb6b197d22950d0d558
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-09-10 15:10:17 +0200

    Fixed error message

--- server/tests/test_cases/search.py
+++ server/tests/test_cases/search.py
@@ -236,7 +236,7 @@ class TestSearch(helpers.TestCase):
     epidb = EpidbClient()
     self.init(epidb)
     s, e = epidb.search("hg19", "genome", self.admin_key)
-    self.assertEqual("genome is not a valid type. The valid types are: 'annotations,bio_sources,epigenetic_marks,experiments,genomes,projects,samples,samples.fields,techniques,tilings'", e)
+    self.assertEqual("genome is not a valid type. The valid types are: 'annotations,bio_sources,column_types,epigenetic_marks,experiments,genomes,projects,samples,samples.fields,techniques,tilings'", e)
 
   def test_search_synonyms(self):
     epidb = EpidbClient()
