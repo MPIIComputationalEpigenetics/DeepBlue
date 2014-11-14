ee6ff16038c3151ceb498560cabab7b559b7cc12
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-11-14 16:23:10 +0100

    Change return type

--- server/src/commands/info.cpp
+++ server/src/commands/info.cpp
@@ -43,7 +43,7 @@ namespace epidb {
       static Parameters results_()
       {
         Parameter p[] = {
-          Parameter("information", serialize::MAP, "Array of Maps containing the objects information", true)
+          Parameter("information", serialize::LIST, "List of Maps containing the objects information", true)
         };
         Parameters results(&p[0], &p[0] + 1);
         return results;
