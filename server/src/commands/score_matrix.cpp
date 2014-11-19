bb2e4ea5d2ffd1dd658ea04fb57213c167b0f2fb
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-11-19 15:38:40 +0100

    Useful score matrix

--- server/src/commands/score_matrix.cpp
+++ server/src/commands/score_matrix.cpp
@@ -35,13 +35,12 @@ namespace epidb {
       static Parameters parameters_()
       {
         Parameter p[] = {
-          Parameter("experiments_id", serialize::STRING, "id of the query", true),
-          Parameter("column_name", serialize::STRING, "column name"),
+          Parameter("experiments_format", serialize::MAP, "id of the query"),
           Parameter("aggregation_function", serialize::STRING, "aggregation function"),
           Parameter("regions_query_id", serialize::STRING, "regions query id"),
           parameters::UserKey
         };
-        Parameters params(&p[0], &p[0] + 5);
+        Parameters params(&p[0], &p[0] + 4);
         return params;
       }
 
@@ -60,10 +59,9 @@ namespace epidb {
       virtual bool run(const std::string &ip,
                        const serialize::Parameters &parameters, serialize::Parameters &result) const
       {
-        const std::string column_name = parameters[1]->as_string();
-        const std::string aggregation_function = parameters[2]->as_string();
-        const std::string regions_query_id = parameters[3]->as_string();
-        const std::string user_key = parameters[4]->as_string();
+        const std::string aggregation_function = parameters[1]->as_string();
+        const std::string regions_query_id = parameters[2]->as_string();
+        const std::string user_key = parameters[3]->as_string();
 
         bool ok = false;
         std::string msg;
@@ -81,19 +79,19 @@ namespace epidb {
           return false;
         }
 
-
-        std::string short_column;
-        if (!dba::KeyMapper::to_short(column_name, short_column, msg)) {
-          result.add_error(msg);
+        // TODO Check same experiments biosource
+        std::map<std::string, serialize::ParameterPtr> map_;
+        if (!parameters[0]->children(map_)) {
+          result.add_error("unable to read metadata");
+          return false;
+        }
+        std::vector<std::pair<std::string, std::string>> experiments_formats;
+        std::map<std::string, serialize::ParameterPtr>::iterator mit;
+        for (mit = map_.begin(); mit != map_.end(); ++mit) {
+          std::pair<std::string, std::string> p(mit->first, mit->second->as_string());
+          experiments_formats.push_back(p);
         }
 
-        // TODO Check same experiments biosource
-
-        std::vector<serialize::ParameterPtr> experiment_names;
-        parameters[0]->children(experiment_names);
-        std::vector<std::string> experiments = dba::helpers::build_vector(experiment_names);
-
-        ChromosomeRegionsList regions;
 
         ChromosomeRegionsList range_regions;
         if (!dba::query::retrieve_query(user_key, regions_query_id, range_regions, msg)) {
@@ -102,15 +100,26 @@ namespace epidb {
         }
 
         /// Experiments, Chromosomes, Values
-        std::vector<std::string> norm_experiments_names;
         std::string norm_genome;
-        for (auto &experiment_name : experiments) {
+
+        // Change names to norm_names and columns
+        std::vector<std::pair<std::string, std::string>> norm_experiments_formats;
+        for (auto &experiment_input : experiments_formats) {
           mongo::BSONObj experiment;
+          const std::string &experiment_name = experiment_input.first;
           if (!dba::experiments::by_name(experiment_name, experiment, msg)) {
             result.add_error(msg);
             return false;
           }
-          norm_experiments_names.push_back(experiment["norm_name"].String());
+          std::string norm_name = experiment["norm_name"].String();
+
+          std::string short_column;
+          if (!dba::KeyMapper::to_short(experiment_input.second, short_column, msg)) {
+            result.add_error(msg);
+          }
+
+          std::pair<std::string, std::string> p(norm_name, short_column);
+          norm_experiments_formats.push_back(p);
 
           // TODO: check if are all from the same genome
           norm_genome = experiment["norm_genome"].String();
@@ -123,9 +132,9 @@ namespace epidb {
           for (auto &region : *chromosome.second) {
             // Get the accumulator from each experiment
             std::vector<algorithms::Accumulator> experiments_accs;
-            for (auto &experiment_name : norm_experiments_names) {
+            for (auto &experiment_format : norm_experiments_formats) {
               mongo::BSONObj regions_query;
-              if (!dba::query::build_experiment_query(region.start(), region.end(), experiment_name, user_key, regions_query, msg)) {
+              if (!dba::query::build_experiment_query(region.start(), region.end(), experiment_format.first, user_key, regions_query, msg)) {
                 result.add_error(msg);
                 return false;
               }
@@ -136,7 +145,7 @@ namespace epidb {
               }
               algorithms::Accumulator acc;
               for (auto &experiment_region : *regions) {
-                acc.push(experiment_region.value(short_column));
+                acc.push(experiment_region.value(experiment_format.second));
               }
               experiments_accs.push_back(acc);
             }
@@ -148,6 +157,18 @@ namespace epidb {
 
 
         std::stringstream ss;
+        ss << "CHROMOSOME\t";
+        ss << "START\t";
+        ss << "END\t";
+        bool first = true;
+        for (auto &experiments_formats : experiments_formats) {
+          if (!first) {
+            ss << "\t";
+          }
+          ss << experiments_formats.first;
+          first = false;
+        }
+        ss << std::endl;
         for (std::map<std::string, std::vector<std::pair<Region, std::vector<algorithms::Accumulator>>>>::iterator chromosome_accs_it = chromosome_accs.begin(); chromosome_accs_it != chromosome_accs.end(); chromosome_accs_it++ ) {
           const std::string &chromosome = chromosome_accs_it->first;
           std::vector<std::pair<Region, std::vector<algorithms::Accumulator>>> &region_accs = chromosome_accs_it->second;
