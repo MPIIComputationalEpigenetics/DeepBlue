90697fca0bbfff6648bf2fc30572c32e0c039cfa
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-12-18 17:34:26 +0100

    StringBuilder writes tmp file to the disk

--- server/src/extras/stringbuilder.hpp
+++ server/src/extras/stringbuilder.hpp
@@ -1,5 +1,5 @@
 //
-//  stringbuilder.cpp
+//  stringbuilder.hpp
 //  epidb
 //
 //  Created by Felipe Albrecht on 01.12.14.
@@ -8,61 +8,29 @@
 
 // Based on http://www.codeproject.com/Articles/647856/Performance-Improvement-with-the-StringBuilde
 
-#include <numeric>
-#include <deque>
+#ifndef EPIDB_STRINGBUILDER_HPP_
+#define EPIDB_STRINGBUILDER_HPP_
 
+#include <fstream>
 
-template <typename chr>
-class StringBuilder {
-    typedef std::basic_string<chr> string_t;
-    typedef std::deque<string_t> container_t;
-    typedef typename string_t::size_type size_type;
-    container_t m_Data;
-    size_type m_totalSize;
+namespace epidb {
+  class StringBuilder {
+    const std::string tempstr;
+    std::ofstream ofs;
 
     StringBuilder(const StringBuilder &);
-    StringBuilder & operator = (const StringBuilder &);
+    StringBuilder &operator = (const StringBuilder &);
 
-public:
-    StringBuilder(const string_t &src) {
-        if (!src.empty()) {
-            m_Data.push_back(src);
-        }
-        m_totalSize = src.size();
-    }
-    StringBuilder() {
-        m_totalSize = 0;
-    }
+  public:
+    StringBuilder();
+    ~StringBuilder();
 
-    void append(const string_t &src) {
-        m_Data.push_back(src);
-        m_totalSize += src.size();
-    }
+    void append(const std::string &src);
+    void append(std::string &&src);
+    void tab();
+    void endLine();
+    std::string to_string();
+  };
+}
 
-    void append(string_t&& src) {
-        m_Data.push_back(src);
-        m_totalSize += src.size();
-    }
-
-    void tab() {
-        static std::string tab("\t");
-        m_Data.push_back(tab);
-        m_totalSize += 1;
-    }
-
-    void endLine() {
-        static std::string new_line("\n");
-        m_Data.push_back(new_line);
-        m_totalSize += 2;
-    }
-
-    string_t ToString() const {
-        string_t result;
-        result.reserve(m_totalSize + 1);
-        for (auto iter = m_Data.begin(); iter != m_Data.end(); ++iter) {
-            result += *iter;
-        }
-        return result;
-    }
-
-}; // class StringBuilder
+#endif
