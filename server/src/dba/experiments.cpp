f592e61749ef8943db15b01c0ac1abb114b5c641
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-12-18 17:36:24 +0100

    Move build metadata

--- server/src/dba/experiments.cpp
+++ server/src/dba/experiments.cpp
@@ -13,6 +13,7 @@
 #include "../datatypes/column_types_def.hpp"
 
 #include "collections.hpp"
+#include "info.hpp"
 #include "helpers.hpp"
 #include "queries.hpp"
 
@@ -52,7 +53,7 @@ namespace epidb {
         for (auto column : experiment_columns) {
           if (column["name"].String() == column_name) {
             pos = column["pos"].Int();
-            const std::string& type_name = column["column_type"].str();
+            const std::string &type_name = column["column_type"].str();
             type = datatypes::column_type_name_to_type(type_name);
             break;
           }
@@ -88,6 +89,72 @@ namespace epidb {
 
         return true;
       }
+
+      bool build_metadata(const std::string &name, const std::string &norm_name,
+                                     const std::string &genome, const std::string &norm_genome,
+                                     const std::string &epigenetic_mark, const std::string &norm_epigenetic_mark,
+                                     const std::string &sample_id, const std::string &technique, const std::string &norm_technique,
+                                     const std::string &project, const std::string &norm_project,
+                                     const std::string &description, const std::string &norm_description,
+                                     const datatypes::Metadata &extra_metadata,
+                                     const std::string &user_key, const std::string &ip,
+                                     const parser::FileFormat &format,
+                                     int &dataset_id,
+                                     std::string &experiment_id,
+                                     mongo::BSONObj &experiment_metadata,
+                                     std::string &msg)
+      {
+        int e_id;
+        if (!helpers::get_counter("experiments", e_id, msg))  {
+          return false;
+        }
+        experiment_id = "e" + utils::integer_to_string(e_id);
+
+        if (!helpers::get_counter("datasets", dataset_id, msg))  {
+          return false;
+        }
+
+        mongo::BSONObjBuilder experiment_data_builder;
+        experiment_data_builder.append("_id", experiment_id);
+        experiment_data_builder.append(KeyMapper::DATASET(), dataset_id);
+        experiment_data_builder.append("name", name);
+        experiment_data_builder.append("norm_name", norm_name);
+        experiment_data_builder.append("genome", genome);
+        experiment_data_builder.append("norm_genome", genome);
+        experiment_data_builder.append("epigenetic_mark", epigenetic_mark);
+        experiment_data_builder.append("norm_epigenetic_mark", norm_epigenetic_mark);
+        experiment_data_builder.append("sample_id", sample_id);
+        experiment_data_builder.append("technique", technique);
+        experiment_data_builder.append("norm_technique", norm_technique);
+        experiment_data_builder.append("project", project);
+        experiment_data_builder.append("norm_project", norm_project);
+        experiment_data_builder.append("description", description);
+        experiment_data_builder.append("norm_description", norm_description);
+        experiment_data_builder.append("format", format.format());
+
+        experiment_data_builder.append("columns", format.to_bson());
+
+        mongo::BSONObj extra_metadata_obj = datatypes::extra_metadata_to_bson(extra_metadata);
+        experiment_data_builder.append("extra_metadata", extra_metadata_obj);
+
+        std::map<std::string, std::string> sample_data;
+        if (!info::get_sample_by_id(sample_id, sample_data, msg, true)) {
+          return false;
+        }
+        mongo::BSONObjBuilder sample_builder;
+        std::map<std::string, std::string>::iterator it;
+        for (it = sample_data.begin(); it != sample_data.end(); ++it) {
+          if ((it->first != "_id") && (it->first != "user")) {
+            sample_builder.append(it->first, it->second);
+          }
+        }
+        experiment_data_builder.append("sample_info", sample_builder.obj());
+
+        experiment_metadata = experiment_data_builder.obj();
+
+        return true;
+      }
+
     }
   }
 }
--- server/src/dba/experiments.hpp
+++ server/src/dba/experiments.hpp
@@ -12,8 +12,11 @@
 #include <string>
 
 #include "../datatypes/column_types_def.hpp"
+#include "../datatypes/metadata.hpp"
 
 #include "../extras/utils.hpp"
+
+#include "../parser/parser_factory.hpp"
 
 namespace epidb {
   namespace dba {
@@ -21,6 +24,20 @@ namespace epidb {
       bool by_name(const std::string &name, mongo::BSONObj &experiment, std::string &msg);
       bool get_field_pos(const DatasetId &dataset_id, const std::string &column_name, int &pos, datatypes::COLUMN_TYPES &type, std::string &msg);
       bool get_field_pos(const std::string &experiment_name, const std::string &column_name, int &pos, datatypes::COLUMN_TYPES &type, std::string &msg);
+
+      bool build_metadata(const std::string &name, const std::string &norm_name,
+                          const std::string &genome, const std::string &norm_genome,
+                          const std::string &epigenetic_mark, const std::string &norm_epigenetic_mark,
+                          const std::string &sample_id, const std::string &technique, const std::string &norm_technique,
+                          const std::string &project, const std::string &norm_project,
+                          const std::string &description, const std::string &norm_description,
+                          const datatypes::Metadata &extra_metadata,
+                          const std::string &user_key, const std::string &ip,
+                          const parser::FileFormat &format,
+                          int &dataset_id,
+                          std::string &experiment_id,
+                          mongo::BSONObj &experiment_metadata,
+                          std::string &msg);
 
       // TODO: move others functions from dba.hpp to here
     }
