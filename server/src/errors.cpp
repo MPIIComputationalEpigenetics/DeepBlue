901523e81997b604695cf33afb5aea066d1f1aaf
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-02-08 19:37:49 +0100

    Variables scope

--- server/src/errors.cpp
+++ server/src/errors.cpp
@@ -66,15 +66,14 @@ namespace epidb {
 
   std::string Error::m(const Error e, ...)
   {
-    int final_n, n = ((int)e.err_fmt.size()) * 2; /* reserve 2 times as much as the length of the fmt_str */
-    std::string str;
+    int n = ((int)e.err_fmt.size()) * 2; /* reserve 2 times as much as the length of the fmt_str */
     boost::shared_ptr<char[]> formatted;
     va_list ap;
     while (1) {
       formatted.reset(new char[n]); /* wrap the plain char array into the unique_ptr */
       strcpy(&formatted[0], e.err_fmt.c_str());
       va_start(ap, e);
-      final_n = vsnprintf(&formatted[0], n, e.err_fmt.c_str(), ap);
+      int final_n = vsnprintf(&formatted[0], n, e.err_fmt.c_str(), ap);
       va_end(ap);
       if (final_n < 0 || final_n >= n)
         n += abs(final_n - n + 1);
