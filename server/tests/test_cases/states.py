a95d980f29568409f6a9582c7533ad4bd0713878
fiaxh <github@lightrise.org>
2015-04-04 01:36:34 +0200

    get_state command unit-test: Refactoring, more examples used

--- server/tests/test_cases/states.py
+++ server/tests/test_cases/states.py
@@ -3,42 +3,130 @@ import data_info
 
 from client import EpidbClient
 
+
 class TestState(helpers.TestCase):
 
-    def get_state(self, vocab, epidb):
-        self.assertSuccess(epidb.get_state(vocab, self.admin_key))
-        return epidb.get_state(vocab, self.admin_key)
 
-    def test_state(self):
+    def test_state_experiments(self):
+        """Tests state after insert and remove for experiments."""
         epidb = EpidbClient()
-        self.init(epidb)
+        self.init_base(epidb)
 
-        summaries = [[data_info.GENOMES, "genomes", self.insert_genome],
-                     [data_info.ANNOTATIONS, "annotations", self.insert_annotation],
-                     [data_info.TECHNIQUES, "techniques", self.insert_technique],
-                     [data_info.BIOSOURCES, "biosources", self.insert_biosource],
-                     [data_info.EPIGENETIC_MARKS, "epigenetic_marks", self.insert_epigenetic_mark],
-                     [data_info.SAMPLES, "samples", self.insert_samples],
-                     [data_info.PROJECTS, "projects", self.insert_project],
-                     [data_info.EXPERIMENTS, "experiments", self.insert_experiment]]
+        name = "experiments"
+        data = self._get_data_names()[name]["data"]
+        insert = self._get_data_names()[name]["insert"]
 
-        for summary in summaries:
-            data = summary[0]
-            vocab = summary[1]
-            insert_function = summary[2]
+        ids = self.insert_all(epidb, data, name, insert)
 
-            s, n1 = self.get_state(vocab, epidb)
-            key, value = data.items()[0]
-            insert_function(epidb, key)
-            s, n2 = self.get_state(vocab, epidb)
-            self.assertTrue(n1 + 1 == n2)
-            key, value = data.items()[1]
-            id = insert_function(epidb, key)
-            s, n3 = self.get_state(vocab, epidb)
-            self.assertTrue(n2 + 1 == n3)
+        self.remove(epidb, "experiments", ids)
 
-            epidb.remove(id, self.admin_key)
-            s, n4 = self.get_state(vocab, epidb)
-            self.assertTrue(n3 + 1 == n4)
+    def insert_random(self, epidb, data, vocab, insert):
+        """Inserts data adding numbers to the name, checks state
+        :param epidb: DeepBlue instance
+        :param data: Data to be inserted
+        :param vocab: Data-type
+        :param insert: Function which inserts the data
+        :return: DeepBlue IDs of inserted data
+        """
+        ids = []
+        state_old = self.get_state(vocab, epidb)
+        key, value = data.items()[0]
 
-            insert_function(epidb, key)
+        for i in range(0, 7):
+            new_key = key + str(i)
+            data[new_key] = value
+            ids.append(insert(epidb, new_key))
+            state_new = self.get_state(vocab, epidb)
+            self.assertTrue(state_old + 1 == state_new)
+            state_old = state_new
+
+        return ids
+
+    def insert_all(self, epidb, data, name, insert):
+        """Insert all data from data_info, checks state
+        :param epidb: DeepBlue instance
+        :param data: Data to be inserted
+        :param name: Data-type
+        :param insert: Function which inserts the data
+        :return: DeepBlue IDs of inserted data
+        """
+        ids = []
+        state_old = self.get_state(name, epidb)
+        for item in data.items():
+            key, value = item
+            ids.append(insert(epidb, key))
+            state_new = self.get_state(name, epidb)
+            self.assertTrue(state_old + 1 == state_new)
+            state_old = state_new
+        return ids
+
+    def remove(self, epidb, name, ids):
+        """
+
+        :param epidb: DeepBlue instance
+        :param name: Data-type
+        :param ids: DeepBlue IDs to be removed
+        """
+
+        state_old = self.get_state(name, epidb)
+        for _id in ids:
+            epidb.remove(_id, self.admin_key)
+            state_new = self.get_state(name, epidb)
+            self.assertTrue(state_old + 1 == state_new)
+            state_old = state_new
+
+    def get_states(self, epidb, exceptions):
+        """States from all data-types except some
+        :param epidb: DeepBlue instance
+        :param exceptions: data-types not to be included
+        :return: List of states
+        """
+        states = []
+        for key in self._get_data_names().keys():
+            if key not in exceptions:
+                states.append(self.get_state(key, epidb))
+        return states
+
+    def get_state(self, name, epidb):
+        """Calls get_state, asserts success
+        :param name: Data-type
+        :param epidb: DeepBlue instance
+        :return: state
+        """
+        self.assertSuccess(epidb.get_state(name, self.admin_key))
+        return epidb.get_state(name, self.admin_key)[1]
+
+    def _get_data_names(self):
+        return {
+            "genomes": {
+                "data": data_info.GENOMES,
+                "insert": self.insert_genome
+            },
+            "annotations": {
+                "data": data_info.ANNOTATIONS,
+                "insert": self.insert_annotation
+            },
+            "techniques": {
+                "data": data_info.TECHNIQUES,
+                "insert": self.insert_technique
+            },
+            "epigenetic_marks": {
+                "data": data_info.EPIGENETIC_MARKS,
+                "insert": self.insert_epigenetic_mark
+            },
+            "projects": {
+                "data": data_info.PROJECTS,
+                "insert": self.insert_project
+            },
+            "biosources": {
+                "data": data_info.BIOSOURCES,
+                "insert": self.insert_biosource
+            },
+            "samples": {
+                "data": data_info.SAMPLES,
+                "insert": self.insert_samples
+            },
+            "experiments": {
+                "data": data_info.EXPERIMENTS,
+                "insert": self.insert_experiment
+            }}
