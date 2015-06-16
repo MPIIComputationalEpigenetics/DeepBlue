8157a2766b2486cbc48b1aa59b9d410386cc0849
fiaxh <github@lightrise.org>
2015-06-16 21:48:56 +0200

    database.cpp -> users.cpp

---
+++ server/src/entities/users.hpp
@@ -0,0 +1,22 @@
+/*
+ * Created by Natalie Wirth on 13.06.2015.
+ * Copyright (c) 2015 Max Planck Institute for Computer Science. All rights reserved.
+ */
+
+#ifndef USER_HPP
+#define	USER_HPP
+
+#include "../datatypes/user.hpp"
+
+namespace epidb {
+  namespace dba {
+    bool add_user(datatypes::User& user, std::string& msg);
+    bool add_new_user(datatypes::User& user, std::string& msg);
+    bool get_user_by_key(const std::string& key, datatypes::User& user, std::string& msg);
+    bool get_user_by_email(const std::string& email, datatypes::User& user, std::string& msg);
+    bool get_user_by_id(const std::string& id, datatypes::User& user, std::string& msg);
+    bool remove_user(const datatypes::User& user, std::string& msg);
+  }
+}
+#endif	/* USER_HPP */
+
