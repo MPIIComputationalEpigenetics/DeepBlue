22b568d5340744cca82229ce8074aecf4d222962
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-07-08 01:05:08 +0200

    update co authors

--- server/src/version.cpp
+++ server/src/version.cpp
@@ -13,7 +13,7 @@ namespace epidb {
   const std::string Version::name = "DeepBlue Epigenomic Data Server";
   const std::string Version::copyright = "Copyright Copyright (c) 2013,2014 Max Planck Institute for Computer Science. All rights reserved.";
   const std::string Version::author = "Felipe Albrecht - felipe.albrecht@mpi-inf.mpg.de";
-  const std::string Version::coauthors = "Fabian Reinartz";
+  const std::string Version::coauthors = "Fabian Reinartz, Natalie Wirth";
   const size_t Version::major_version = 0;
   const size_t Version::minor_version = 9;
   const size_t Version::fix_version = 72;
