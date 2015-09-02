254a2ec66021cdeb685a573137826bf8fd3231f4
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-09-02 17:47:02 +0200

    version 1.0.2

--- server/src/version.cpp
+++ server/src/version.cpp
@@ -16,5 +16,5 @@ namespace epidb {
   const std::string Version::coauthors = "Fabian Reinartz, Natalie Wirth";
   const size_t Version::major_version = 1;
   const size_t Version::minor_version = 0;
-  const size_t Version::fix_version = 1;
+  const size_t Version::fix_version = 2;
 }
