204c6b1ef9e62ecb6483eb3660225e33b7d1f3c1
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-09-11 01:29:15 +0200

    Simple test for gene_sets: in progress

--- server/tests/test_cases/gene_sets.py
+++ server/tests/test_cases/gene_sets.py
@@ -10,6 +10,15 @@ class TestAnnotationCommands(helpers.TestCase):
     self.init_base(epidb)
 
     data = open("data/gtf/gencode.v23.basic.annotation_head.gtf").read()
-    print data
 
+    print "inserting..."
     print epidb.add_gene_set("Test One", "Test One Description", data, "GTF", {}, self.admin_key)
+
+
+    (s, query_id) = epidb.select_genes(["ENSG00000223972.5", "ENSG00000223972.5", "DDX11L1"], "Test One", self.admin_key)
+
+    print epidb.get_regions(query_id, "CHROMOSOME,START,END", self.admin_key)
+
+    import time
+    time.sleep(0.1)
+    print epidb.get_request_data("r1", self.admin_key)
