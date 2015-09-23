8c746cccd538b002c6f57a940d346c5f2eb0a707
Felipe Albrecht <=>
2015-09-23 17:33:41 +0200

    super ot fix

--- server/src/dba/retrieve.cpp
+++ server/src/dba/retrieve.cpp
@@ -254,7 +254,7 @@ namespace epidb {
 
         mongo::Query query = mongo::Query(regions_query)
                              .sort(BSON(KeyMapper::START() << 1 << KeyMapper::END() << 1))
-                             .hint("S_1_E_1");
+                             .hint("D_1_S_1_E_1");
 
         int queryOptions = (int)( mongo::QueryOption_NoCursorTimeout | mongo::QueryOption_SlaveOk );
 
