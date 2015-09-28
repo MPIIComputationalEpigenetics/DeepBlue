1a470e9cca34a23a08b5b823a9ab2a618bf3aae9
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-09-28 17:55:58 +0100

    Remove some warnings during compilator

--- server/src/lua/sandbox.hpp
+++ server/src/lua/sandbox.hpp
@@ -47,44 +47,6 @@ namespace epidb {
 
       static void MaximumInstructionsReached(lua_State *, lua_Debug *);
     };
-
-    static const char *LUA_ENV =
-      "-- sample sandbox environment\n"
-      "sandbox_env = {\n"
-      " print = print,\n"
-      " value_of = value_of,\n"
-      " ipairs = ipairs,\n"
-      " next = next,\n"
-      " pairs = pairs,\n"
-      " pcall = pcall,\n"
-      " tonumber = tonumber,\n"
-      " tostring = tostring,\n"
-      " type = type,\n"
-      " unpack = unpack,\n"
-      " string = { byte = string.byte, char = string.char, find = string.find,\n"
-      "   format = string.format, gmatch = string.gmatch, gsub = string.gsub,\n"
-      "   len = string.len, lower = string.lower, match = string.match,\n"
-      "   rep = string.rep, reverse = string.reverse, sub = string.sub,\n"
-      "   upper = string.upper },\n"
-      " table = { insert = table.insert, maxn = table.maxn, remove = table.remove,\n"
-      " sort = table.sort },\n"
-      " math = { abs = math.abs, acos = math.acos, asin = math.asin,\n"
-      " atan = math.atan, atan2 = math.atan2, ceil = math.ceil, cos = math.cos,\n"
-      " cosh = math.cosh, deg = math.deg, exp = math.exp, floor = math.floor,\n"
-      " fmod = math.fmod, frexp = math.frexp, huge = math.huge,\n"
-      " ldexp = math.ldexp, log = math.log, log10 = math.log10, max = math.max,\n"
-      " min = math.min, modf = math.modf, pi = math.pi, pow = math.pow,\n"
-      " rad = math.rad, random = math.random, sin = math.sin, sinh = math.sinh,\n"
-      " sqrt = math.sqrt, tan = math.tan, tanh = math.tanh },\n"
-      " os = { clock = os.clock, difftime = os.difftime, time = os.time },\n"
-      "}\n";
-
-
-    static const char *LUA_FUNCTION_HEADER = "function row_value()\n";
-
-    static const char *LUA_FUNCTION_FOOTER = "\nend";
-
-    static const char *LUA_SET_SANDBOX = "setfenv(row_value, sandbox_env)\n";
   }
 }
 
