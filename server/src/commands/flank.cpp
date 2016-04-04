d34d1bfca1a819c2dcd52a0af67c654dcd56350a
Felipe Albrecht <felipe.albrecht@gmail.com>
2016-04-04 18:12:07 +0200

    Formating file.

--- server/src/commands/flank.cpp
+++ server/src/commands/flank.cpp
@@ -38,7 +38,7 @@ namespace epidb {
     private:
       static CommandDescription desc_()
       {
-        return CommandDescription(categories::OPERATIONS, "Generate flanking regions for the given regions.");
+        return CommandDescription(categories::OPERATIONS, "Generate flanking regions from the given regions.");
       }
 
       static Parameters parameters_()
@@ -111,6 +111,3 @@ namespace epidb {
     } flankCommand;
   }
 }
-
-
-
