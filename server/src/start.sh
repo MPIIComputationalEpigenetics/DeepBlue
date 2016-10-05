05519e2d0eb83d6abef3d42cf3e1913483cd5584
Felipe Albrecht <felipe.albrecht@gmail.com>
2016-10-05 14:43:48 +0200

    Update start.sh
    Dont go to background.

--- server/src/start.sh
+++ server/src/start.sh
@@ -3,4 +3,4 @@ export LD_LIBRARY_PATH=/local/data/DeepBlue/server/third_party/luajit-2.0/src/
    echo "Starting DeepBlue"  | mutt -s "DeepBlue is starting" albrecht@mpi-inf.mpg.de
    (nohup ./server -A 0.0.0.0 -P 56573 -D deepblue_1_0_0_pre_2  -R 8 -T 8 -M localhost:27027 2>&1; echo \"PID $! exited with status $?\") >> deepblue.log
    echo "DeepBlue crashed"  | mutt -s "DeepBlue crashed" albrecht@mpi-inf.mpg.de
-done) &
+done)
