90243f97fff0142e812c82d152b3ce2e015ba2ad
Felipe Albrecht <felipe.albrecht@gmail.com>
2017-06-27 03:20:22 +0900

    more LOLA code.

--- server/src/processing/lola.cpp
+++ server/src/processing/lola.cpp
@@ -23,6 +23,7 @@
 
 #include "../algorithms/algorithms.hpp"
 
+#include "../dba/experiments.hpp"
 #include "../dba/genomes.hpp"
 #include "../dba/genes.hpp"
 #include "../dba/gene_ontology.hpp"
@@ -36,6 +37,16 @@
 #include <algorithm> // for min and max
 
 using namespace std;
+
+
+template<int M, template<typename> class F = std::less>
+struct TupleCompare {
+  template<typename T>
+  bool operator()(T const &t1, T const &t2)
+  {
+    return F<typename tuple_element<M, T>::type>()(std::get<M>(t1), std::get<M>(t2));
+  }
+};
 
 namespace epidb {
   namespace processing {
@@ -77,6 +88,17 @@ namespace epidb {
       std::vector<std::string> chromosomes(chromosomes_s.begin(), chromosomes_s.end());
 
       auto databases_it = databases.begin();
+
+
+      std::vector<std::tuple<std::string, size_t>> datasets_support;
+      std::vector<std::tuple<std::string, double>> datasets_log_score;
+      std::vector<std::tuple<std::string, double>> datasets_odds_score;
+
+
+      // --------------------- dataset_id, dataseset_name, database_name, negative_natural_log, 0, a, b, c, d,)
+      std::vector<std::tuple<std::string, std::string, std::string, double, double, double, double, double, double>> results;
+
+
       while ( databases_it.more() ) {
         const mongo::BSONElement &database = databases_it.next();
         const std::string& database_name = std::string(database.fieldName());
@@ -93,6 +115,13 @@ namespace epidb {
           if (!dba::query::build_experiment_query(-1, -1, experiment_name, regions_query, msg)) {
             return false;
           }
+
+          mongo::BSONObj experiment_obj;
+          if (!dba::experiments::by_name(experiment_name, experiment_obj, msg)) {
+            return false;
+          }
+
+          const std::string dataset_id = experiment_obj["_id"].str();
 
           ChromosomeRegionsList reg;
           if (!dba::retrieve::get_regions(genome, chromosomes, regions_query, false, status, reg, msg, /* reduced_mode */ true)) {
@@ -130,11 +159,119 @@ namespace epidb {
 
           std::cerr <<"(" << a << ", " << b << ", " << c << ", " << d << ") log odds: ";
 
-          std::cerr << math::fisher_test(a, b, c, d) << " time: ";
+          double p_value = math::fisher_test(a, b, c, d);
+          double negative_natural_log = abs(log(p_value));
+
+          std::cerr << negative_natural_log << " ";
 
           long diffticks = clock() - times;
-          std::cerr << ((diffticks) / (CLOCKS_PER_SEC / 1000)) << std::endl;
+          std::cerr << " time: " << ((diffticks) / (CLOCKS_PER_SEC / 1000)) << std::endl;
+
+          datasets_support.push_back(std::make_tuple(dataset_id, a));
+          datasets_log_score.push_back(std::make_tuple(dataset_id, negative_natural_log));
+          datasets_odds_score.push_back(std::make_tuple(dataset_id, 0));
+
+          /*
+           @return Data.table with enrichment results.
+           Rows correspond to individual pairwise fisher's tests comparing a single userSet with a single databaseSet.
+
+           The columns in this data.table are:
+            - userSet
+            - and dbSet: index into their respective input region sets.
+            - pvalueLog: -log10(pvalue) from the fisher's exact result;
+            - oddsRatio: result from the fisher's exact test;
+            - support: number of regions in userSet overlapping databaseSet;
+            - rnkPV: rank in this table of p-value
+            - rnkLO: rank in this table of oddsRatio
+            - rnkSup: rank in this table of Support respectively.
+            - The --value is the negative natural log of the p-value returned from a one-sided fisher's exact test.
+            - maxRnk: max of the 3 previous ranks, providing a combined ranking system
+            - meanRnk: mean of the 3 previous ranks, providing a combined ranking system.
+            - b: other value completing the 2x2 contingency table (with support).
+            - c: other value completing the 2x2 contingency table (with support).
+            - d: other value completing the 2x2 contingency table (with support).
+          */
+
+
+          results.push_back(std::make_tuple(dataset_id, experiment_name, database_name, negative_natural_log, 0, a, b, c, d));
         }
+      }
+
+      std::unordered_map<std::string, size_t> datasets_support_rank;
+      std::sort(begin(datasets_support), end(datasets_support), TupleCompare<0>());
+      for(size_t i = 0; i < datasets_support.size(); i++) {
+        datasets_support_rank[get<0>(datasets_support[i])] = i;
+      }
+      //
+
+      std::unordered_map<std::string, size_t> datasets_log_rank;
+      std::sort(begin(datasets_log_score), end(datasets_log_score), TupleCompare<0>());
+      for(size_t i = 0; i < datasets_log_score.size(); i++) {
+        datasets_log_rank[get<0>(datasets_log_score[i])] = i;
+      }
+      //
+
+      std::unordered_map<std::string, size_t> datasets_odd_rank;
+      std::sort(begin(datasets_odds_score), end(datasets_odds_score), TupleCompare<0>());
+      for(size_t i = 0; i < datasets_odds_score.size(); i++) {
+        datasets_odd_rank[get<0>(datasets_odds_score[i])] = i;
+      }
+
+      struct ExperimentResult {
+        std::string dataset_id;
+        std::string experiment_name;
+        std::string database_name;
+
+        double negative_natural_log;
+        double oddsRatio;
+
+        double a;
+        double b;
+        double c;
+        double d;
+
+        size_t support_rank;
+        size_t log_rank;
+        size_t odd_rank;
+
+        size_t max_rank;
+        double mean_rank;
+      };
+
+      struct ERCompare {
+        bool operator()(std::shared_ptr<ExperimentResult> const &er, std::shared_ptr<ExperimentResult> const &er2)
+        {
+          return er->mean_rank < er2->mean_rank;
+        }
+      };
+
+      std::vector<std::shared_ptr<ExperimentResult>> experiment_results;
+
+      for (const auto& result : results) {
+        auto er = std::make_shared<ExperimentResult>();
+        er->dataset_id = get<0>(result);
+        er->experiment_name = get<1>(result);
+        er->database_name = get<2>(result);
+        er->negative_natural_log = get<3>(result);
+        er->oddsRatio = get<4>(result);
+        er->a = get<5>(result);
+        er->b = get<6>(result);
+        er->c = get<7>(result);
+        er->d = get<8>(result);
+
+        er->support_rank = datasets_support_rank[er->dataset_id];
+        er->log_rank = datasets_log_rank[er->dataset_id];
+        er->odd_rank = datasets_odd_rank[er->dataset_id];
+        er->max_rank = std::max(er->support_rank, std::max(er->log_rank, er->odd_rank));
+        er->mean_rank = (er->support_rank + er->log_rank + er->odd_rank) / 3.0;
+
+        experiment_results.emplace_back(std::move(er));
+      }
+
+      std::sort(begin(experiment_results), end(experiment_results), ERCompare());
+
+      for (const auto& er: experiment_results) {
+        std::cerr << er->dataset_id << " "  << er->experiment_name << " support rank: " << er->support_rank << " mean_rank: " << er->mean_rank << std::endl;
       }
 
       return true;
